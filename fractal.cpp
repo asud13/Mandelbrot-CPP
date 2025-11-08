09a0c4f592f818d8e30435cfb5f1631965a583f3
asud13 <147271553+asud13@users.noreply.github.com>
2025-11-08 11:16:50 -0600

    Comments for me to understand lol

--- fractal.cpp
+++ fractal.cpp
@@ -4,24 +4,31 @@
 #include <iostream>
 
 int main() {
+
+    // SDL INIT VIDEO: 
+    // Initializes SDL video subsystem where you create windows, renderers and graphic displays 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cerr << "SDL_Init failed: " << SDL_GetError() << "\n";
         return 1;
     }
 
+    // Sample width and height that are adjustable by SDL_WINDO_RESIZABLE 
     int width = 900;
     int height = 600;
 
-    SDL_Window* window = SDL_CreateWindow("Mandelbrot Canvas",
+    SDL_Window* window = SDL_CreateWindow("Mandelbrot Canvas", 
                                           width, height,
                                           SDL_WINDOW_RESIZABLE);
+
+
     if (!window) {
         std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << "\n";
         SDL_Quit();
         return 1;
     }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, 0); // creates renderer in window object with no other attributes, hence the 0
+
     if (!renderer) {
         std::cerr << "SDL_CreateRenderer failed: " << SDL_GetError() << "\n";
         SDL_DestroyWindow(window);
@@ -29,19 +36,22 @@ int main() {
         return 1;
     }
 
+    // Creation of texture given renderer object, width and height, other feilds give attributes to given texture 
     SDL_Texture* texture = SDL_CreateTexture(renderer,
                                              SDL_PIXELFORMAT_RGBA8888,
                                              SDL_TEXTUREACCESS_STREAMING,
                                              width, height);
 
-    std::vector<uint32_t> pixels(width * height, 0);
+    std::vector<uint32_t> pixels(width * height, 0); // Initiallize dynamically changing pixels 
+
+    // Now the fun stuff, the running of the actual window... 
 
     bool running = true;
     SDL_Event event;
 
     while (running) {
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT)
+            if (event.type == SDL_EVENT_QUIT) // User may exit at any time
                 running = false;
 
             if (event.type == SDL_EVENT_WINDOW_RESIZED) {
@@ -57,6 +67,8 @@ int main() {
             }
         }
 
+        // We update the texture with given dynamically allocated pixel vector 
+
         SDL_UpdateTexture(texture, nullptr, pixels.data(), width * sizeof(uint32_t));
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
@@ -64,6 +76,9 @@ int main() {
         SDL_RenderPresent(renderer);
         SDL_Delay(10);
     }
+
+
+    // Standard destructors on close 
 
     SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
